61961ed137350995a8340df72509556ca6b3ec98
luiz <luiz_tauffer@hotmail.com>
2024-07-09 14:45:44 +0200

    blackrock probe

--- src/nelson_lab_to_nwb/blackrock_sessions_converter/blackrock_converter.py
+++ src/nelson_lab_to_nwb/blackrock_sessions_converter/blackrock_converter.py
@@ -1,6 +1,9 @@
+from typing import Dict
 from neuroconv.datainterfaces import BlackrockRecordingInterface, BlackrockSortingInterface, VideoInterface
 from nelson_lab_to_nwb.interfaces import CognitiveBehavioralInterface
 from neuroconv import NWBConverter
+
+from nelson_lab_to_nwb.utils.probe import set_probe
 
 
 class BlackrockNWBConverter(NWBConverter):
@@ -16,3 +19,10 @@ class BlackrockNWBConverter(NWBConverter):
         BehavioralEvents=CognitiveBehavioralInterface,
         BehavioralVideo=VideoInterface,
     )
+
+    def __init__(self, source_data: Dict[str, dict], verbose: bool = True):
+        super().__init__(source_data=source_data, verbose=verbose)
+
+        # Add probe information: https://probeinterface.readthedocs.io/en/main/index.html
+        set_probe(self.data_interface_objects["BlackrockRaw"].recording_extractor)
+        set_probe(self.data_interface_objects["BlackrockLFP"].recording_extractor)
