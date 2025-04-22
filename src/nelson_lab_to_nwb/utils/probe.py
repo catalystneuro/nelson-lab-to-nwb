3092aeeb3dc57b7ebcd087885a87e349133e1f64
luiz <luiz_tauffer@hotmail.com>
2025-04-22 12:38:09 +0200

    probe new

--- src/nelson_lab_to_nwb/utils/probe.py
+++ src/nelson_lab_to_nwb/utils/probe.py
@@ -1,8 +1,9 @@
-from probeinterface import Probe
+from typing import Literal
+from probeinterface import Probe, generate_multi_shank
 import numpy as np
 
 
-def set_probe(extractor):
+def set_probe_type_1(extractor) -> None:
     positions = [[450, 0], [600, 0]]
     positions.extend([[150, 150], [300, 150], [450, 150], [600, 150], [750, 150], [900, 150]])
     positions.extend(
@@ -37,3 +38,53 @@ def set_probe(extractor):
     )
     probe.set_device_channel_indices(channel_indices=np.arange(0, 32))
     extractor.set_probe(probe, in_place=True)
+
+
+def set_probe_type_2(extractor) -> None:
+    probe = generate_multi_shank(
+        num_shank=2,
+        num_columns=2,
+        num_contact_per_column=16,
+        shank_pitch=[250., 0],
+    )
+
+    positions = np.zeros((64, 2))
+    for i in range(0, 16):
+        positions[i] = (0, i * 25)
+    for i in range(16, 32):
+        positions[i] = (22.5, (i-16) * 25 + 12.5)
+    for i in range(32, 48):
+        positions[i] = (250, (i-32) * 25)
+    for i in range(48, 64):
+        positions[i] = (275.5, (i-48) * 25 + 12.5)
+
+    probe.set_contacts(
+        positions=positions,
+        shapes="rect",
+        shape_params={"width": 11, "height": 15},
+        contact_ids=np.arange(0, 64),
+    )
+
+    extractor.set_probe(probe, in_place=True)
+
+
+def set_probe(
+    extractor,
+    probe_type: Literal["type_1", "type_2"] = "type_1",
+) -> None:
+    """
+    Set the probe for the given extractor.
+
+    Parameters
+    ----------
+    extractor : object
+        The extractor object to set the probe for.
+    probe_type : str
+        The type of probe to set. Can be "type_1" or "type_2".
+    """
+    if probe_type == "type_1":
+        set_probe_type_1(extractor)
+    elif probe_type == "type_2":
+        set_probe_type_2(extractor)
+    else:
+        raise ValueError(f"Unknown probe type: {probe_type}")
